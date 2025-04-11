7cd4fa635c8e130acd3295438ad24796a3c300fa
Vadym Hulchenko <vadym.hulchenko@redspace.com>
2025-04-11 14:29:00 -0300

    improve login page position to support smaller screens, update gitignore

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import SignUpForm from "@/app/login/SignUpForm";
 
 const LoginPage = async () => {
   return (
-    <div className="mx-auto pt-32 lg:pt-60 items-center flex flex-col text-xl">
+    <div className="flex flex-col items-center justify-start min-h-screen pt-32 text-xl">
       <h1 className="text-3xl lg:text-6xl p-6">
         Lead your healthy <span className="text-teal-700">life</span> here!
       </h1>
